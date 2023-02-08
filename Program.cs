b4ee37840fc726bcbdc189209bb54e4798a82a1e
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-08 07:47:28 +0530

    Add: First program added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Helloworld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CheckLeapYear.Leapyear();
+            //FlipCoin.CalculatePercentage();
+            //PowerOf2.PrintPowerOf2();
+            //Harmonic.PrintHarmonic();
+            PrimeFactors.PrintPrimeFactors();
+
+        }
+    }
+}
